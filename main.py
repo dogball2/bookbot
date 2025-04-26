fb970acbf7fbaeaf97f031f611e19516146514d1
Dogball <unshapenabyss@gmail.com>
2025-04-26 11:39:47 -0400

    finish book path and completed project

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import sys
+def check_book():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -5,12 +12,27 @@ def get_book_text(filepath):
 
 from stats import get_num_words
 from stats import counting_characters
+from stats import chars_dict_to_sorted_list
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    check_book()
+    text = get_book_text(sys.argv[1])
     word_count = get_num_words(text)
     count = counting_characters(text)
-    print(f"{word_count} words found in the document")
-    print(count)
+    sorted_list = chars_dict_to_sorted_list(count)
+    sorted_alpha = []
+    for item in sorted_list:
+        if item["char"].isalpha():
+            sorted_alpha.append(item)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}")
+    print(f"----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    
+    print(f"--------- Character Count -------")
+    for item in sorted_alpha:
+        print(f"{item['char']}: {item['count']}")
+        
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,4 +10,13 @@ def counting_characters(text):
             char_count[letter] += 1
         else:
             char_count[letter] = 1
-    return char_count           
+    return char_count    
+
+def chars_dict_to_sorted_list(char_dict):
+    chars_list = []
+    for char, count in char_dict.items():
+        chars_list.append({"char":char, "count":count})
+    def sort_on(dict):
+        return dict["count"]
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
